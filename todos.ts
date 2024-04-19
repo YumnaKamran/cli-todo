7962996714c321f81876690da5e0aac71b288ea7
YumnaKamran <160340248+YumnaKamran@users.noreply.github.com>
2024-04-19 06:51:07 -0700

    Add files via upload

---
+++ todos.ts
@@ -0,0 +1,27 @@
+#! /usr/bin/env node
+import inquirer from "inquirer"
+
+let todos = [];
+
+let condition =true;
+
+while(condition){
+
+let addTask = await inquirer.prompt([
+{
+    name : "firstName",
+    type : "input",
+    message : "What would you like to add in  your todos?"
+},
+{
+    name : "secondQuestion",
+    type : "confirm",
+    message : "Would you like to add more in your todo?",
+}
+
+])
+todos.push(addTask.firstName)
+console.log(todos)
+condition = addTask.secondQuestion
+
+}
